e22f2c5c88409626f5db13eba0ba3dcaefe85967
shadowruge <zambelestar@gmail.com>
2022-07-13 23:50:48 -0300

    renew script

--- script.js
+++ script.js
@@ -1,47 +1,80 @@
-function salvar() {
-    document.getElementById("service").value
-    document.getElementById("modelo").value
-    document.getElementById("placa").value
-    document.getElementById("valor").value
+const service = document.getElementById('service')
+const model = document.getElementById('model')
+const license_plate = document.getElementById('license_plate')
+const price = document.getElementById('price')
+const total = parseInt(document.getElementById('total').value)
 
-    dayName = new Array("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
-    monName = new Array("Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
-    now = new Date
-    fuldate = {
-      hora: now.getHours(),
-      minutos: now.getMinutes(),
-      dia: dayName[now.getDay()],
-      data: now.getDate(),
-      mes: now.getMonth() + 1,
-      ano: now.getFullYear()
-    }
+const fullDate = () => {
   
-    patio =
-      `<div>
-    <table border=",1">
-    <tr>
-      <td>Serviço</td>
-      <td>Modelo</td>
-      <td>Placa</td>
-      <td>Valor</td>
-      <td>Dia</td>
-      <td>Data</td>
-      <td>Mês</td>
-      <td>Ano</td>
-    </tr>
-    
-    <tr>
-      <td> ${service.value}</td>
-      <td> ${modelo.value}</td>
-      <td> ${placa.value}</td>
-      <td> ${valor.value}</td>
-      <td>${dayName[now.getDay()]}</td>
-      <td>${now.getDate()}</td>
-      <td> ${monName[now.getMonth()]}</td>
-      <td>${now.getFullYear()}</td>
-    </tr>
-    </table>
+//Days of the week 
+  dayName = new Array(
+    "domingo",
+    "segunda",
+    "terça",
+    "quarta",
+    "quinta",
+    "sexta",
+    "sábado")
 
-    </div>`
-    document.getElementById("view").innerHTML += patio
+//Months of the year
+  monName = new Array(
+    "Janeiro",
+    "Fevereiro",
+    "Marco",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro")
+
+//Corrent date
+  now = new Date
+  fulldate = {
+    hora: now.getHours(),
+    minutos: now.getMinutes(),
+    dia: dayName[now.getDay()],
+    data: now.getDate(),
+    mes: now.getMonth() + 1,
+    ano: now.getFullYear()
   }
+
+}
+
+//Cars in the yard
+const noPatio = () => {
+  patioIndom =
+    `<li>
+    Serviço: ${service.value} 
+    Modelo: ${model.value}
+    Placa: ${license_plate.value}
+    Valor: ${price.value}
+    Dia: ${dayName[now.getDay()]}
+    Data: ${now.getDate()}
+    Mês: ${monName[now.getMonth()]}
+    Ano: ${now.getFullYear()}
+    Hora: ${now.getHours()}
+    Minutos: ${now.getMinutes()}
+    <hr>
+  </li>`
+
+  //Elements displayed in the Dom 
+  document.getElementById("view").innerHTML += patioIndom;
+}
+
+const soma=()=>{
+const a = [parseInt(document.getElementById('price').value)];
+const total = a.reduce((acumulator, item)=>acumulator+=item);
+console.log(total);
+
+}
+
+const salvar=()=> {
+
+  fullDate()
+  noPatio()
+  soma()  
+}
